ceffa448435877af8ab12b6e342c16c5f368b1e8
lawrenceChege <lawrencechege.s@gmail.com>
2018-08-11 04:12:51 +0300

    update home

--- UI/js/main.js
+++ UI/js/main.js
@@ -26,3 +26,11 @@ function classToggle() {
 function editfunction() {
     var x = document.getElementsByClassName("editprofile")[0].style.display = "block";
 }
+function myFunction(id) {
+    var x = document.getElementById(id);
+    if (x.className.indexOf("show") == -1) {
+        x.className += " show";
+    } else { 
+        x.className = x.className.replace(" show", "");
+    }
+}
