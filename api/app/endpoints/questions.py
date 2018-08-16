00cf06478bdb46f66762d1023b6f83c62077b5c6
lawrenceChege <lawrencechege.s@gmail.com>
2018-08-16 10:25:05 +0300

    fix update method

--- api/app/endpoints/questions.py
+++ api/app/endpoints/questions.py
@@ -1,4 +1,5 @@
 """ defines CRUD methods for questions """
+import datetime
 from flask import Blueprint, jsonify, request
 from flask_restful import fields, marshal_with, reqparse, Resource
 from app.helpers.resources import QNS
@@ -18,22 +19,9 @@ POST_PARSER.add_argument(
     type=str, location='form',
     required=True, help='The question body',
 )
-QUESTION_FIELDS = {
-    "question_id": fields.Integer,
-    "user_id": fields.Integer,
-    "Title": fields.String,
-    "Body": fields.String,
-    "date_created": fields.DateTime,
-    "date_modified": fields.DateTime,
-    "upvotes": 3,
-    "downvotes": 1,
-    "answers": fields.Integer
-}
-
 
 class Questions(Resource):
     """ methods for questions"""
-    @marshal_with(QUESTION_FIELDS)
     def post(self):
         """
         Posts a new question.
@@ -66,20 +54,20 @@ class Questions(Resource):
         if 'body' in request.json and not request.json['body']:
             return {"message": "body must be a string."}, 400
 
-        title, body, = request.json['title'], request.json['body']
-        qns = [qns for qns in QNS if qns['question_id'] == 0]
+        title, body, question_id = request.json['title'], request.json['body'], request.json['question_id']
         single_qn = {
-            'question_id': qns['id'] + 1,
+            'question_id': question_id,
             "Title": title,
             "Body": body,
-            "date_created": fields.DateTime,
-            "date_modified": fields.DateTime,
+            "date_created": str(datetime.datetime.now()),
+            "date_modified": str(datetime.datetime.now()),
             "upvotes": 0,
             "downvotes": 0,
             "answers": 0,
         }
-        return QNS.append(single_qn)
-
+        QNS.append(single_qn)
+        return jsonify({"message": "Question posted successfully!"},
+                       {"sigle_qn": single_qn})
     def get(self):
         """get all questsions"""
         return jsonify({"message": "all requests found successfully"}, {"questions": QNS})
@@ -113,7 +101,6 @@ class Qusetion(Resource):
         single_qn = [single_qn for single_qn in QNS if single_qn['question_id'] == question_id]
         return single_qn
 
-    @marshal_with(QUESTION_FIELDS)
     def put(self, question_id):
         """
         Modifies a request.
@@ -143,17 +130,16 @@ class Qusetion(Resource):
         404:
             description: Page not found.
         """
-        single_qn = [single_qn for single_qn in QNS if single_qn['question_id'] == question_id]
-        if 'title' in request.json and not isinstance(request.json['title'], str):
-            return jsonify({"message": "Title should be a string"})
-        if 'body' in request.json and not isinstance(request.json['body'], str):
-            return jsonify({"message": "Question body is a string"})
-
-        single_qn[0]['title'] = request.json.get('title', single_qn[0]['title']),
-        single_qn[0]['description'] = request.json.get(
-            'description', single_qn[0]['description']),
+        si_qn = [si_qn for si_qn in QNS if si_qn['question_id'] == question_id]
+        if 'title' in request.json and not request.json['title']:
+            return {"message": "Please enter a title"}, 400
+        if 'body' in request.json and not request.json['body']:
+            return {"message": "body must be a string."}, 400
+        si_qn[0]['Title'] = request.json.get('title', si_qn[0]['Title']),
+        si_qn[0]['Body'] = request.json.get('body', si_qn[0]['Body']),
+        si_qn[0]['date_modified'] = str(datetime.datetime.now())
         return jsonify({"message": "Question successfully updated"},
-                       {"single_qn": single_qn})
+                       {"single_qn": si_qn})
 
     def delete(self, question_id):
         """
