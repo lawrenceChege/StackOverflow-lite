f099bb24e9e63abce10157ae2be9393bf0cdea53
lawrenceChege <lawrencechege.s@gmail.com>
2018-08-16 07:35:15 +0300

    [ft 159813719] add question blue prints and routes

--- api/app/app.py
+++ api/app/app.py
@@ -1,3 +1,13 @@
 from flask import Flask
+from flask_restful import Api
+from flasgger import Swagger
+from endpoints.questions import questions, Qusetion, Questions
+
 
 app = Flask(__name__)
+api = Api(app)
+swagger = Swagger(app)
+app.register_blueprint(questions)
+api.add_resource(Questions,'/api/v1/questions/')
+api.add_resource(Qusetion, '/api/v1/questions/<int:question_id>')
+
