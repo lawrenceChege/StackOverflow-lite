e8c5faa91c58030624b8e17fe5356866bc5af59f
lawrencechege <lawrencechege.s@gmail.com>
2018-08-18 18:59:03 +0300

    add resources

---
+++ api/resources/resources.py
@@ -0,0 +1,85 @@
+""" holds resources for the app """
+QNS = [
+    {
+        "question_id": 1,
+        "user_id": 1,
+        "title": "how are you doing?",
+        "body": "fogort hammer",
+        "date_created": 11/3/18,
+        "date_modified": 12/3/18,
+        "upvotes": 3,
+        "downvotes": 1,
+        "answers": 0,
+    },
+    {
+        "question_id": 2,
+        "user_id": 1,
+        "title": "how are you doing?",
+        "body": "fogort hammer",
+        "date_created": 11/3/18,
+        "date_modified": 12/3/18,
+        "upvotes": 3,
+        "downvotes": 1,
+        "answers": 0,
+    },
+    {
+        "question_id": 3,
+        "user_id": 2,
+        "title": "how are you doing?",
+        "body": "fogort hammer",
+        "date_created": 11/3/18,
+        "date_modified": 12/3/18,
+        "upvotes": 3,
+        "downvotes": 1,
+        "answers": 0,
+    },
+    {
+        "question_id": 4,
+        "user_id": 1,
+        "title": "how are you doing?",
+        "body": "fogort hammer",
+        "date_created": 11/3/18,
+        "date_modified": 12/3/18,
+        "upvotes": 3,
+        "downvotes": 1,
+        "answers": 0,
+    }
+]
+ANS = [
+    {
+        "answer_id": 1,
+        "user_id": 1,
+        "question_id": 2,
+        "body": "i wish i knew",
+        "date_created": 11/3/18,
+        "date_modified": 12/3/18,
+        "upvotes": 3,
+        "downvotes": 1,
+        "answers": 0,
+        "status": "Pending"
+    },
+    {
+        "answer_id": 2,
+        "user_id": 1,
+        "question_id": 2,
+        "body": "nobody knows",
+        "date_created": 11/3/18,
+        "date_modified": 12/3/18,
+        "upvotes": 3,
+        "downvotes": 1,
+        "answers": 0,
+        "status": "Pending"
+    },
+    {
+        "answer_id": 3,
+        "user_id": 1,
+        "question_id": 2,
+        "body": "things are better",
+        "date_created": 11/3/18,
+        "date_modified": 12/3/18,
+        "upvotes": 3,
+        "downvotes": 1,
+        "answers": 0,
+        "status": "Pending"
+    }
+]
