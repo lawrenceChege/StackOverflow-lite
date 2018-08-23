a8e8e143770e2f14ec0c48713297ad813e05b4bd
lawrencechege <lawrencechege.s@gmail.com>
2018-08-23 08:19:26 +0300

    update methods for questions

--- api/helpers/helper_questions.py
+++ api/helpers/helper_questions.py
@@ -33,7 +33,7 @@ class HelperDb(object):
                 return {"message":"Question posted successfully!"}, 201
         except(Exception, psycopg2.DatabaseError) as error:
             print(error)
-            return {"message": error},400
+            return {"message": error}, 400
 
     def update_request(self, question_id, single_question):
         """elper for updating a question"""
@@ -43,14 +43,14 @@ class HelperDb(object):
             question_i = self.cur.fetchone()
             if question_i:
                 self.cur.execute(
-                    "UPDATE questions SET title=%(title)s, body=%(bodyy)s, date_modified=%(date_modified)s", data)
+                    "UPDATE questions SET title=%(title)s, body=%(body)s, date_modified=%(date_modified)s", single_question)
                 self.conn.commit()
                 return {"message":"Request updated successfully!"}, 200
             else:
                 return {"message":"Request does not exist"}, 400
         except(Exception, psycopg2.DatabaseError) as error:
             print(error)
-            return {"message": error},400
+            return {"message": error}, 400
 
     def delete_request(self, question_id):
         """handles deleting the question from database"""
@@ -62,9 +62,9 @@ class HelperDb(object):
                 self.cur.execute(
                     """ DELETE FROM questions WHERE question_id = %s""", (question_id,))
                 self.conn.commit()
-                return {"message":"Request deleted successfully!"},200
+                return {"message":"Request deleted successfully!"}, 200
             else:
-                return {"message":"Request does not exitst!"},400
+                return {"message":"Request does not exitst!"}, 400
         except(Exception, psycopg2.DatabaseError) as error:
             print(error)
             return {"message": error}, 400
