e565f0f0b1f5be5c940899b0cdf4cfeb9e0828e6
lawrenceChege <lawrencechege.s@gmail.com>
2018-09-04 12:42:41 +0300

    add configuration to run.py

--- run.py
+++ run.py
@@ -1,6 +1,9 @@
+import os
 from api.app import APP
+from instance.config import app_config
 from migrations import create_tables
 
+APP.config.from_object(app_config[os.getenv('APP_SETTINGS')])
 create_tables()
 
 if __name__ == '__main__':
